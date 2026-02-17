74508a3b00cdb95517ef6ff9abf10c618b6276e5
rameshkumarm535 <rameshkumarm535@gmail.com>
2026-02-17 18:33:57 +0530

    Update glue_job.py

--- aws_glue_etl/glue_job.py
+++ aws_glue_etl/glue_job.py
@@ -1,1 +1,30 @@
+import sys
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Read from S3
+datasource = glueContext.create_dynamic_frame.from_options(
+    connection_type="s3",
+    connection_options={"paths": ["s3://your-bucket/input/"]},
+    format="csv"
+)
+
+# Transform
+df = datasource.toDF()
+df_clean = df.dropna()
+
+# Write to S3
+df_clean.write.mode("overwrite").parquet("s3://your-bucket/output/")
+
+job.commit()
 

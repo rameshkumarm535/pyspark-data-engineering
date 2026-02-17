0ab02bea345a06ada0944ca33a512611c0c3cfd3
rameshkumarm535 <rameshkumarm535@gmail.com>
2026-02-17 18:31:30 +0530

    Update kafka_example.py

--- streaming/kafka_example.py
+++ streaming/kafka_example.py
@@ -1,1 +1,17 @@
+from pyspark.sql import SparkSession
 
+spark = SparkSession.builder \
+    .appName("KafkaStreaming") \
+    .getOrCreate()
+
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "test-topic") \
+    .load()
+
+query = df.writeStream \
+    .format("console") \
+    .start()
+
+query.awaitTermination()

8a65f08e27715d7cb39808d8d4475754d23764c2
rameshkumarm535 <rameshkumarm535@gmail.com>
2026-02-17 18:40:34 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
 
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "data_bucket" {
+  bucket = "test-data-bucket"
+}
+
+resource "aws_glue_catalog_database" "glue_db" {
+  name = "data_engineering_db"
+}

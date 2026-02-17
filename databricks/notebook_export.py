bdf804202f1e20ea9b173ed877cdeb494482c7f8
rameshkumarm535 <rameshkumarm535@gmail.com>
2026-02-17 18:44:42 +0530

    Delete databricks directory

--- databricks/notebook_export.py
+++
@@ -1,9 +0,0 @@
-# Databricks Notebook Example
-
-from pyspark.sql.functions import current_timestamp
-
-df = spark.read.format("delta").load("/mnt/delta/sales")
-
-df_transformed = df.withColumn("processed_time", current_timestamp())
-
-df_transformed.write.format("delta").mode("overwrite").save("/mnt/delta/sales_gold")
